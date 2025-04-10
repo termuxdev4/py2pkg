9047e648b732e283c03791da32a254140aea91b9
termuxdev4 <playingguitarsinmobile@gmail.com>
2025-04-10 19:36:22 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
-#!/bin/bash
-if ! command -v wget >/dev/null 2>&1; then
+#!/data/data/com.termux/files/usr/bin/bash
+
+if ! command -v wget > /dev/null 2>&1; then
   pkg install wget -y
 fi
-wget https://raw.githubusercontent.com/termuxdev4/py2pkg/refs/heads/main/src/py2pkg.py
-mv py2pkg.py /data/data/com.termux/files/usr/bin/py2pkg
-chmod +x /data/data/com.termux/files/usr/bin/py2pkg
-echo "py2pkg is successfully installed!"
+
+cd ~
+
+wget -O py2pkg.py https://raw.githubusercontent.com/termuxdev4/py2pkg/refs/heads/main/src/py2pkg.py
+
+if ! grep -q "alias py2pkg=" ~/.bashrc; then
+  echo "alias py2pkg='python3 py2pkg.py'" >> ~/.bashrc
+fi
+
+echo "py2pkg is successfully installed! At version 2.0.0"
